5acb87e99b3f545b1562afba4737b116d4f6490f
fromwu <wulijun@walkclass.com>
2019-03-03 20:06:45 +0800

    StringEquals

---
+++ StringEquals.java
@@ -0,0 +1,19 @@
+
+public class StringEquals {
+    public static void main(String []args) {
+       System.out.println("Hello World");
+       String s1 = "Canada";        //String作为基本类型，基本类型==比较的是值
+       String s2 = "Canada";
+
+       String s3= new String(s1);       //String为对象，引用类型，引用类型==比较的是堆荐地埴；equals比较的是值
+       String s4= new String("Canada"); 
+       
+       if(s1 == s2) System.out.println("s1 == s2");
+       if(s1 == s3) System.out.println("s1 == s3");
+       if(s3 == s4) System.out.println("s3 == s4");
+      
+       if(s1.equals(s2)) System.out.println("s1.equals(s2)");
+       if(s1.equals(s3)) System.out.println("s1.equals(s3)");
+       if(s1.equals(s4)) System.out.println("s1.equals(s4)");
+    }
+}
